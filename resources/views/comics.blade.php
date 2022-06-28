5415f063af70c5641c4f7c87be2e50e8614b30be
mohamedslima <hammaslima@gmail.com>
2022-06-28 11:06:40 +0200

    added flex and init comics page single

--- resources/views/comics.blade.php
+++ resources/views/comics.blade.php
@@ -6,20 +6,20 @@
 
 @section('main_content')
     <div class="bg-main">
-        <h2>
-            Current Series
-        </h2>
         <div class="container">
+            <h2>
+                Current Series
+            </h2>
             <div class="comics-list">
                 @foreach ($comics as $card)
                     <div class="comics-card">
                         <img src="{{ $card['thumb'] }}" alt="">
                         <h3>
-                            <h3><a href=""></a></h3>
+                            <a href="">{{ $card['title'] }}</a>
                         </h3>
                     </div>
                 @endforeach
             </div>
         </div>
-    </div>    
+    </div>
 @endsection
